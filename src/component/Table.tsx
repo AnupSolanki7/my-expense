df0c0d9b8c3df13cefa535084b9745ee2838132e
AnupWiseSkulls7 <anup@wiseskulls.com>
2023-05-02 17:41:20 +0530

    anup

--- src/component/Table.tsx
+++ src/component/Table.tsx
@@ -1,17 +1,43 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FaHandshake } from "react-icons/fa";
 import { AiFillEdit } from "react-icons/ai";
 import { FcBusinessman, FcBusinesswoman } from "react-icons/fc";
 import styles from "@/styles/Home.module.css";
-import { deleteExpenseID } from "@/service/api";
+import { deleteExpenseID, editExpense } from "@/service/api";
 import moment from "moment";
+import { Button, Input, Modal, Select } from "antd";
 
 const Table = ({ tableData, setTableData, refresh, setRefresh }: any) => {
+  const [formData, setFormData]: any = useState({
+    amount: "",
+    bearer: "",
+    date: new Date(),
+  });
+  const formRef: any = useRef();
+  const [open, setOpen] = useState(false);
   const deleteExpense = (id: any) => {
     deleteExpenseID(id).then((res: any) => {
       setRefresh(!refresh);
     });
+  };
+
+  const onFinish = (e: any) => {
+    e.preventDefault();
+    formRef.current.reportValidity();
+    if (formRef.current.reportValidity()) {
+      editExpense(e.id,formData).then((res: any) => {
+        console.log(res);
+
+        setOpen(false);
+        setRefresh(!refresh);
+        setFormData({
+          amount: "",
+          bearer: "",
+          date: new Date(),
+        });
+      });
+    }
   };
   return (
     <>
@@ -56,8 +82,72 @@ const Table = ({ tableData, setTableData, refresh, setRefresh }: any) => {
               )}
 
               <span>
-                <p style={{ color: "#49ff49", display:"flex", justifyContent:"space-between"}}>₹ {e.amount}.00/- <span style={{color:"blueviolet", cursor:"pointer", fontSize:"19px"}}><AiFillEdit/></span></p>
-                
+                <p
+                  style={{
+                    color: "#49ff49",
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  ₹ {e.amount}.00/-{" "}
+                  <span
+                    style={{
+                      color: "blueviolet",
+                      cursor: "pointer",
+                      fontSize: "19px",
+                    }}
+                  >
+                    <AiFillEdit onClick={() => {}} />
+                    <Modal
+                      title="Add Expense"
+                      rootClassName="add-form"
+                      open={open}
+                      onOk={() => setOpen(false)}
+                      onCancel={() => setOpen(false)}
+                      footer={false}
+                    >
+                      <form ref={formRef}>
+                        <span className="form-item">
+                          <label htmlFor="amount">Amount</label>
+                          <Input
+                            required
+                            value={formData.amount}
+                            prefix="₹"
+                            onChange={(e: any) =>
+                              setFormData({
+                                ...formData,
+                                amount: e.target.value,
+                              })
+                            }
+                            type="text"
+                            id="amount"
+                            placeholder="enter amount"
+                          />
+                        </span>
+                        <span className="form-item">
+                          <label htmlFor="bearer">Bearer</label>
+                          <Select
+                            value={formData.bearer || "select a bearer"}
+                            id="bearer"
+                            placeholder="select a bearer"
+                            style={{ width: "100%" }}
+                            onChange={(e: any) => {
+                              setFormData({ ...formData, bearer: e });
+                            }}
+                            options={[
+                              { value: "Anup", label: "Anup" },
+                              { value: "Aparna", label: "Aparna" },
+                              { value: "Equal", label: "Equal" },
+                            ]}
+                          />
+                        </span>
+
+                        <Button onClick={onFinish}>Add</Button>
+                      </form>
+                    </Modal>
+                  </span>
+                </p>
+
                 <p>{moment(e.date).format("MMMM Do YYYY, h:mm a")}</p>
               </span>
             </div>
--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const URL = "https://my-expense.vercel.app"
-const URL = "http://localhost:3000";
+const URL = "https://my-expense.vercel.app"
+// const URL = "http://localhost:3000";
 
 export async function getExpense() {
   return axios.get(`${URL}/api/expense`).then((res: any) => {
@@ -18,6 +18,12 @@ export async function deleteExpenseID(id: any) {
 
 export async function addExpense(formData: any) {
   return axios.post(`${URL}/api/expense`, formData).then((res: any) => {
+    return res;
+  });
+}
+
+export async function editExpense(id:any,formData: any) {
+  return axios.put(`${URL}/api/expense/?_id=${id}`, formData).then((res: any) => {
     return res;
   });
 }
