1ea69c73ae5e1fd47eca8cd5be77b0910274c592
AnupWiseSkulls7 <anup@wiseskulls.com>
2023-05-02 15:33:43 +0530

    anup

--- src/component/Table.tsx
+++ src/component/Table.tsx
@@ -2,59 +2,43 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { MdDelete } from "react-icons/md";
 import styles from "@/styles/Home.module.css";
+import { deleteExpenseID } from "@/service/api";
 
-const Table = ({refresh, setRefresh}:any) => {
-  const [tableData, setTableData] = useState([]);
-
-
-  useEffect(() => {
-    getExpenses();
-  }, [refresh]);
-
-  const getExpenses = async () => {
-    axios.get("https://my-expense.vercel.app/api/expense").then((res: any) => {
-      setTableData(res.data.expense);
+const Table = ({ tableData, setTableData, refresh, setRefresh }: any) => {
+  const deleteExpense = (id: any) => {
+    deleteExpenseID(id).then((res: any) => {
+      setRefresh(!refresh);
     });
   };
-
-  const deleteExpense = (id: any) => {
-    axios
-      .delete(`https://my-expense.vercel.app/api/expense/?_id=${id}`)
-      .then((res: any) => {
-        setRefresh(!refresh);
-      });
-  };
   return (
-    <div>
-      <table className={styles.table}>
-        <thead>
-          <tr>
-            <th>Amount</th>
-            <th>Bearer</th>
-            <th>Date</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableData.map((e: any) => {
-            return (
-              <tr>
-                <td>₹{e.amount}</td>
-                <td>{e.bearer}</td>
-                <td>{e.date}</td>
-                <td style={{ fontSize: "20px" }}>
-                  {" "}
-                  <MdDelete
-                    style={{ cursor: "pointer" }}
-                    onClick={() => deleteExpense(e._id)}
-                  />{" "}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          <th>Amount</th>
+          <th>Bearer</th>
+          <th>Date</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableData.map((e: any) => {
+          return (
+            <tr>
+              <td>₹{e.amount}</td>
+              <td>{e.bearer}</td>
+              <td>{e.date}</td>
+              <td style={{ fontSize: "20px" }}>
+                {" "}
+                <MdDelete
+                  style={{ cursor: "pointer" }}
+                  onClick={() => deleteExpense(e._id)}
+                />{" "}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
---
+++ src/service/api.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+// const URL = "https://my-expense.vercel.app"
+const URL = "http://localhost:3000";
+
+export async function getExpense() {
+  return axios.get(`${URL}/api/expense`).then((res: any) => {
+    return res;
+  });
+}
+
+export async function deleteExpenseID(id: any) {
+  return axios.delete(`${URL}/api/expense/?_id=${id}`).then((res: any) => {
+    return res;
+  });
+}
